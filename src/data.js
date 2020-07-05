8baea180cb908d2cf442e317820c141b7a20875d
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-07-05 01:35:38 -0400

    sample data

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const boilerPlateData = [
+  {
+    id: 1,
+    name: "Pikachu",
+    strength: 10.5,
+    description: "PIKA PIKA!"
+  },
+  {
+    id: 2,
+    name: "Charmander",
+    strength: 7.3,
+    description: "Fire-type Lizard Pokémon"
+  },
+  {
+    id: 3,
+    name: "Squirtle",
+    strength: 8.0,
+    description: "Launches incredible water attacks with amazing range and accuracy"
+  }
+];
