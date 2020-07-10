3c8f84beea53c02147913f191dd283ee91d8f656
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-07-09 22:10:29 -0400

    Slider setup

---
+++ src/components/Slider.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default Slider = () => {
+    const [state, setState] = useState(0);
+    
+}
+// functions to rotate through slides, continuous loop via state
+// included with onClick event
+// rendering of carousel
